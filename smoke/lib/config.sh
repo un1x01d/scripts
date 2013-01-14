2af93ff8b493eeaf73c8aa87f2deecfcd1b8f764
un1x01d <zedchel@gmail.com>
2013-01-14 14:36:17 -0500

    error checking, mplayer installed

--- smoke/lib/config.sh
+++ smoke/lib/config.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 mplayer_config() {
-mplayerconf="~/.mplayer/config"
-	echo "
+mplayerconf="$HOME/.mplayer/config"
+echo " 
 prefer-ipv4 = yes
 nolirc= yes
-really-quiet= 1
-" >> $mplayerconf
+really-quiet= 1" >> $mplayerconf 
 	}
 
 get_os() {
@@ -14,29 +13,50 @@ get_os() {
 	OS=$NAME
 	}
 
+check_root() {
+	if [ "$(id -u)" != "0" ] 
+		then
+   			echo "This script must be run as root" 1>&2
+  	 			exit 1
+	fi
+	     }
+
+install_mplayer() {
+	#check_root
+	echo "Installing"
+		sleep 1
+	case $OS in 
+		Fedora) 
+			sudo yum install mplayer -y > /dev/null
+		;;	
+		Ubuntu)
+			sudo apt-get install mplayer -y > /dev/null
+		;;
+	esac
+
+		if [  $? = "0" ] 
+			then echo "SUCCESS: Installed"
+			else echo "ERROR: Install Failed"
+		fi
+}
 
 check_mplayer() {
 	get_os
 	MPLAYER="/bin/mplayer"
 	if [ ! -e $MPLAYER ]
-		then echo "Mplayer is not installed!"
-			sleep 1
-			echo "Installing"
-				sleep 1
-				case $OS in 
-			
-					Fedora) 
-						sudo yum install mplayer -y > /dev/null
-						;;	
-					Ubuntu)
-						sudo apt-get install mplayer -y > /dev/null
-		 				;;
+		then read -p "Mplayer is not installed! (y/n) [Default:y]" install_response
+				case $install_response in 
+					y)
+						install_mplayer	
+					;;
+					n)
+						echo "Mplayer is required, exiting."
+							exit 1
+					;;
+					*)	
+						install_mplayer	
+					;;
 				esac
-
-			if [  $? = "0" ] 
-				then echo "SUCCESS: Installed"
-				else echo "ERROR: Install Failed"
-			fi
 
 			if [ -f $mplayerconf ]
 				then echo "Configuring Mplayer."
