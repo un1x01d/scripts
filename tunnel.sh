94fee24117fb69a37787b40ceadc99d8f3d29817
Eugene Chechelnitski <eugenec@viggle.com>
2013-01-15 11:46:56 -0500

    typos in tunnel.sh fixed

--- tunnel.sh
+++ tunnel.sh
@@ -15,9 +15,4 @@ fi
 
 echo "TUNNEL: to $TARGET over $JUMP on local port $PORT"
 
-<<<<<<< HEAD
-ssh -vv -L9091:$TARGET.mgmt:9090 $JUMP -NCf
-=======
 ssh -L9091:$TARGET.mgmt:9090 $JUMP -NCf
->>>>>>> 6d7258e22faa4b02bc1504cef99a2247394081a2
-
