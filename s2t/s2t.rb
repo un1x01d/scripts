eddec88e62077f71772d2191d95bc6e1bcadc374
un1x01d <zedchel@gmail.com>
2013-01-30 16:30:21 -0500

    clean version

--- s2t/s2t.rb
+++ s2t/s2t.rb
@@ -1,7 +1,22 @@
 #!/usr/bin/ruby
 
 require 'speech'
+require 'timeout'
 
-file = ARGV[0]
+p "Speak! it's record."
+sleep(1)
+pid = fork do 
+	exec "./record.py"
+end
+
+sleep(5)
+p pid
+Process.kill("TERM", pid)
+Process.wait pid
+
+sleep(1)
+
+file = "test.wav"
+#file = ARGV[0]
 audio = Speech::AudioToText.new("#{file}")
-puts audio.to_text
+puts audio.to_text.inspect
